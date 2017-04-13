3314c79fa5369730ba1c6654b0b1f5292860f62c
jamesalbert <jalbert1@uci.edu>
2017-04-13 02:05:00 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # halfprec
 Half-precision assembly interface for C
+
+
+##### Synopsis
+
+This library converts 32-bit floats to [IEEE 754-2008](https://en.wikipedia.org/wiki/Half-precision_floating-point_format) half-precision floating point format.
+
+Consider a simple example:
+
+```
+#include...
+
+void half2float(float* __restrict out, uint16_t* in, int n);
+void float2half(uint16_t* __restrict out, float* in, int n);
+
+int main(int argc, char *argv[]) {
+  int i, n = 5;
+  float float1[n], float2[n];
+  unsigned short halfs[n];
+  srand48(time(0)+getpid());
+  for(i=0; i<n; i++)
+    float1[i] = drand48();
+  float2half(halfs, float1, n);
+  half2float(float2, halfs, n);
+  for(i=0; i<n; i++)
+    printf("%f converted to %hu converted to %f\n", float1[i], halfs[i], float2[i]);
+  return 0;
+}
+```
+
+the output would be:
+
+```
+0.142886 converted to 12434 converted to 0.142822
+0.728122 converted to 14803 converted to 0.728027
+0.386010 converted to 13869 converted to 0.385986
+0.002425 converted to 6391 converted to 0.002424
+0.622540 converted to 14586 converted to 0.622070
+```
+
+and all you have to do to run it is:
+
+```
+nasm -felf64 src/halfprec.asm -o obj/halfprec.o;
+gcc -Wall target.c obj/halfprec.o -o program;
+./program
+```
+
+I've currently tested this under:
+  - Operating System: Centos 7
+  - GCC: 4.8.5 20150623 (Red Hat 4.8.5-11)
+  - NASM: 2.10.07
+
+##### Vagrant
+
+There is also a Vagrantfile that provisions a centos 7 environment with all the packages you'd need for development. Bring it up with `vagrant up && vagrant ssh`. The project directory is in `/home/vagrant/halfprec`.
---
+++ src/halfprec.asm
@@ -0,0 +1,155 @@
+; Author: James Albert
+; Mode: 64 bits
+; Syntax: NASM
+; Instruction set: 80386, x64
+
+global half2float
+global float2half
+
+
+SECTION .text
+
+half2float:
+  push rbp
+  mov rbp, rsp
+  mov qword [rbp-18H], rdi
+  mov qword [rbp-20H], rsi
+  mov dword [rbp-24H], edx
+  mov dword [rbp-4H], 0
+  jmp L4
+
+float2half:
+  push rbp
+  mov rbp, rsp
+  mov qword [rbp-28H], rdi
+  mov qword [rbp-30H], rsi
+  mov dword [rbp-34H], edx
+  mov dword [rbp-4H], 0
+  jmp L12
+
+L1:
+  mov eax, dword [rbp-4H]
+  cdqe
+  lea rdx, [rax+rax]
+  mov rax, qword [rbp-20H]
+  add rax, rdx
+  movzx eax, word [rax]
+  movzx eax, ax
+  and eax, 7FFFH
+  mov dword [rbp-8H], eax
+  mov eax, dword [rbp-4H]
+  cdqe
+  lea rdx, [rax+rax]
+  mov rax, qword [rbp-20H]
+  add rax, rdx
+  movzx eax, word [rax]
+  movzx eax, ax
+  and eax, 8000H
+  mov dword [rbp-0CH], eax
+  mov eax, dword [rbp-4H]
+  cdqe
+  lea rdx, [rax+rax]
+  mov rax, qword [rbp-20H]
+  add rax, rdx
+  movzx eax, word [rax]
+  movzx eax, ax
+  and eax, 7C00H
+  mov dword [rbp-10H], eax
+  shl dword [rbp-8H], 13
+  shl dword [rbp-0CH], 16
+  add dword [rbp-8H], 939524096
+  cmp dword [rbp-10H], 0
+  jz L2
+  mov eax, dword [rbp-8H]
+  jmp L3
+
+L2:
+  mov eax, 0
+
+L3:
+  mov dword [rbp-8H], eax
+  mov eax, dword [rbp-0CH]
+  or dword [rbp-8H], eax
+  mov eax, dword [rbp-4H]
+  cdqe
+  lea rdx, [rax*4]
+  mov rax, qword [rbp-18H]
+  add rdx, rax
+  mov eax, dword [rbp-8H]
+  mov dword [rdx], eax
+  add dword [rbp-4H], 1
+
+L4:
+  mov eax, dword [rbp-4H]
+  cmp eax, dword [rbp-24H]
+  jl L1
+  pop rbp
+  ret
+
+L5:
+  mov eax, dword [rbp-4H]
+  cdqe
+  lea rdx, [rax*4]
+  mov rax, qword [rbp-30H]
+  add rax, rdx
+  mov eax, dword [rax]
+  mov dword [rbp-8H], eax
+  mov eax, dword [rbp-8H]
+  and eax, 7FFFFFFFH
+  mov dword [rbp-0CH], eax
+  mov eax, dword [rbp-8H]
+  and eax, 80000000H
+  mov dword [rbp-10H], eax
+  mov eax, dword [rbp-8H]
+  and eax, 7F800000H
+  mov dword [rbp-14H], eax
+  shr dword [rbp-0CH], 13
+  shr dword [rbp-10H], 16
+  sub dword [rbp-0CH], 114688
+  cmp dword [rbp-14H], 947912703
+  jbe L6
+  mov eax, dword [rbp-0CH]
+  jmp L7
+
+L6:
+  mov eax, 0
+
+L7:
+  mov dword [rbp-0CH], eax
+  cmp dword [rbp-14H], 1191182336
+  ja L8
+  mov eax, dword [rbp-0CH]
+  jmp L9
+
+L8:
+  mov eax, 31743
+
+L9:
+  mov dword [rbp-0CH], eax
+  cmp dword [rbp-14H], 0
+  jz L10
+  mov eax, dword [rbp-0CH]
+  jmp L11
+
+L10:
+  mov eax, 0
+
+L11:
+  mov dword [rbp-0CH], eax
+  mov eax, dword [rbp-10H]
+  or dword [rbp-0CH], eax
+  mov eax, dword [rbp-4H]
+  cdqe
+  lea rdx, [rax+rax]
+  mov rax, qword [rbp-28H]
+  add rdx, rax
+  mov eax, dword [rbp-0CH]
+  mov word [rdx], ax
+  add dword [rbp-4H], 1
+
+L12:
+  mov eax, dword [rbp-4H]
+  cmp eax, dword [rbp-34H]
+  jl L5
+  pop rbp
+  ret
